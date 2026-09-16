fcac2d1fd14e93a727b89407289fb22055d66dc7
kmj3648-web <kmj3648@gmail.com>
2026-09-16 21:36:52 +0900

    Update supabase.js

--- -T--main/src/lib/supabase.js
+++ -T--main/src/lib/supabase.js
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js'
 
 // Vercel deployment variables
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL || https://cmpthlnnnpnkeyhkqvmm.supabase.co
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY || sb_publishable_Gqkq7I__6P7YEy3fHT4yZg_DqJEkGOz
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL || 'https://cmpthlnnnpnkeyhkqvmm.supabase.co'
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY || 'sb_publishable_Gqkq7I__6P7YEy3fHT4yZg_DqJEkGOz'
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
